1cd810eb2fe07bc8a65e9cce1a4390faed263fe3
Carlos Oporto <carlosoporto@gmail.com>
2019-04-02 12:18:03 +0200

    added bet function and checkContractBalance

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -1,5 +1,26 @@
 pragma solidity ^0.4.10;
 
+contract Ownable {
+  address owner;
+  function Ownable() public {
+    // Set owner to who creates the contract
+    owner = msg.sender;
+  }
+
+  // Access modifier
+  modifier Owned {
+    require (msg.sender == owner);
+    _;
+  }
+}
+
+contract Mortal is Ownable {
+  // Our access modifier is present, only the contract creator can use this function
+  function kill() public Owned {
+    selfdestruct(owner); // destoy contract and send back funds to owner
+  }
+}
+
 contract Casino is Mortal {
   uint minBet;
   uint houseEdge; // in %
@@ -16,5 +37,22 @@ contract Casino is Mortal {
 
   function() public { //fallback
     revert();
+  }
+
+  function bet(uint _number) payable public {
+    require(_number > 0 && _number <= 10);
+    require(msg.value >= minBet);
+    uint winningNumber = block.number % 10+1;
+    if (_number == winningNumber) {
+      uint amountWon = msg.value * (100 - houseEdge)/10;
+      if(!msg.sender.send(amountWon)) revert();
+      emit Won(true, amountWon);
+    } else {
+      emit Won(false, 0);
+    }
+  }
+
+  function checkContractBalance() Owned public view returns(uint) {
+    return address(this).balance;
   }
 }
--- contracts/Mortal.sol
+++
@@ -1,6 +0,0 @@
-contract Mortal is Ownable {
-  // Our access modifier is present, only the contract creator can use this function
-  function kill() public Owned {
-    selfdestruct(owner); // destoy contract and send back funds to owner
-  }
-}
